c16c2d63204f1b1360644dc066e39e491edce56f
ian.grant <tnargnai@gmail.com>
2016-10-20 14:43:12 +0100

    first commit

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,5 @@ description 'Installs/Configures b_hardening'
 long_description 'Installs/Configures b_hardening'
 version '0.1.0'
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/b_hardening/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/b_hardening' if respond_to?(:source_url)
+depends 'os-hardening'
+depends 'ssh-hardening'
